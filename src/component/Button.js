705c2f95184ae5359811c663a71a86f976efb773
Prajakta <prajakta.pote@agiliad.com>
2024-08-06 09:25:49 +0530

    Saurabh

--- src/component/Button.js
+++ src/component/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({text, type = 'button'}) => {
   return (
-    <button type={type} className='w-full rounded-lg bg-red-500 font-semibold text-base leading-3 text-center py-3 sm:py-4 text-white'>{text}</button>
+    <button type={type} className='w-full rounded-lg bg-[#882EFD] font-semibold text-base leading-3 text-center py-3 sm:py-4 text-white'>{text}</button>
   )
 }
 
